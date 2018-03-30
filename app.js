01138a7ffef5c20e7f375d307b2e84698affb62b
Grace Huo <Grace.Huo@intersystems.com>
2018-03-29 21:23:40 -0400

    Add SQL database

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-var express = require('express');
+var express = require( 'express' );
 
 var app = express(); // get an instance of express
 
-var port = process.env.PORT || 5000;
+var mysql = require('mysql');
+var connection = mysql.createConnection( {
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'Books'
+});
 
-app.use(express.static('public'));
-app.set('views', './src/views');
+connection.connect(function(err){
+  if(!err) {
+    console.log("Database is connected ...");
+  } else {
+    console.log("Error connecting database ...");
+  }
+});
+
+
+var port       = process.env.PORT || 5000;
+
+var nav = [
+  { Link: '/Books', Text: 'Book' },
+  { Link: '/Authors', Text: 'Author' }
+];
+
+
+var bookRouter = require('./src/routes/bookRoutes')(nav, connection);
+
+app.use( express.static( 'public' ) );
+app.set( 'views', './src/views' );
 
 // var handlebars = require('express-handlebars');
 // app.engine('.hbs', handlebars({extname: '.hbs'}));
@@ -13,16 +38,21 @@ app.set('views', './src/views');
 // app.set('view engine', 'jade');
 // app.set('view engine', '.hbs');
 
-app.set('view engine', 'ejs');
+app.set( 'view engine', 'ejs' );
 
-app.get('/', function(req, res) {
-  res.render('index', {title: "Hello", list: ['a', 'b']});
-});
+app.use( '/Books', bookRouter );
 
-app.get('/books', function(req, res) {
-  res.send('Hello!');
-});
+app.get( '/', function( req, res ) {
+  res.render( 'index', {
+    title: "Hello",
+    nav  : nav
+  } );
+} );
 
-app.listen(port, function( err ) {
+app.get( '/books', function( req, res ) {
+  res.send( 'Hello!' );
+} );
+
+app.listen( port, function( err ) {
   console.log( 'running server on port ' + port );
-});
+} );
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,54 @@
+var express    = require( 'express' );
+var bookRouter = express.Router();
+var sql        = require( 'mssql' );
+
+var router = function( nav, connection ) {
+  bookRouter.route( '/' )
+    .get( function( req, res ) {
+      connection.query( 'SELECT * FROM books', function( err, rows, fields ) {
+        if ( !err ) {
+          var books = JSON.parse( JSON.stringify( rows ) );
+          res.render( "bookListView", {
+            title: "Books",
+            nav  : nav,
+            books: books
+          } );
+        }
+        else {
+          console.log( 'Error while performing Query.', err );
+        }
+      } );
+    } );
+
+  bookRouter.route( '/:id' )
+    .all( function( req, res, next ) {
+      var id  = req.params.id;
+      var sql = 'SELECT * FROM books WHERE id = ?';
+      connection.query( sql, [id], function( err, result ) {
+        if ( !err ) {
+          if (result.length === 0) {
+            res.status(404).send('Not Found');
+          }
+          else {
+            req.book = JSON.parse( JSON.stringify( result ) )[0];
+            next();
+          }
+        }
+        else {
+          console.log( 'Error while performing Query.', err );
+        }
+      } )
+    } )
+    .get( function( req, res ) {
+      res.render( "bookView", {
+        title: "Book",
+        nav  : nav,
+        book : req.book
+      } );
+    } );
+
+return bookRouter;
+};
+
+
+module.exports = router;
